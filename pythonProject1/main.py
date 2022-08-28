29559a26ddaadc53c09becda774d4c8d6274000e
eberne <eberne@att.net>
2022-08-28 15:49:00 -0700

    Modified the print_hi function

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -6,10 +6,10 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+Shift+B to toggle the breakpoint.
+    print(f'Hi, {name}. How are you?')  # Press Ctrl+Shift+B to toggle the breakpoint.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Ed')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+exit(0)
